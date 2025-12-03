36bafad037d913cf7cd82f1a6fabc331029d843a
francis-gedoz <flgedoz@gmail.com>
2025-12-02 21:24:27 -0300

    creando navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import NetworksPage from "./Pages/NetworksPage";
 import StationsPage from "./Pages/StationsPage";
@@ -7,6 +8,7 @@ import StationsPage from "./Pages/StationsPage";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/networks" element={<NetworksPage />} />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return <nav>Navbar</nav>;
+    return (
+        <nav style={{ padding: "10px", background: "#ddd" }}>
+        <Link to="/">Home</Link> | <Link to="/networks">Redes</Link>
+        </nav>
+    );
 }
 
 export default Navbar;
