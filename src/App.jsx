968180bbe6be7291f14d405072dc85a5a23764d5
francis-gedoz <flgedoz@gmail.com>
2025-12-02 21:20:09 -0300

    creando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import NetworksPage from "./Pages/NetworksPage";
+import StationsPage from "./Pages/StationsPage";
 
 function App() {
-  return <h1>GlobalBikes</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/networks" element={<NetworksPage />} />
+        <Route path="/stations/:id" element={<StationsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
